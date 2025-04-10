a8fe59d88544674410dd8c3689f08b77c8fd86a5
Ayush Jain <ayushj820@gmail.com>
2025-04-10 21:36:39 +0530

    Adding Initial Setup

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-gray-900 text-white text-4xl font-bold">
+      Hello, Tailwind! 🚀
+    </div>
+
+    </>
+  )
+}
+
+export default App
